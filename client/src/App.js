59f539f604d83327feb99e5070de1f0bdf4df9f8
Krishnaveni mandula <krishnavenimandula123@gmail.com>
2024-10-05 00:32:35 +0100

    created initial project folders

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import ToDoList from "./todo-list";
+
+const App = () => {
+  return (
+    <div>
+      <ToDoList />
+    </div>
+  );
+};
+
+export default App;
---
+++ server/go.mod
@@ -0,0 +1,3 @@
+module github.com/RahulPali78/golang-todo
+
+go 1.23.2
